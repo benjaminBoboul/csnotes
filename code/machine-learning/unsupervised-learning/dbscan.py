869dad135e5e09ab21aa66d1d622f0c0843f7449
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2021-04-21 23:51:13 +0200

    Update python code samples

--- code/machine-learning/unsupervised-learning/dbscan.py
+++ code/machine-learning/unsupervised-learning/dbscan.py
@@ -19,6 +19,11 @@ boston_dataframe = DataFrame(pca_x_train)
 
 print(boston_dataframe)
 
+# eps: The maximum distance between two samples for one to be considered as in the neighborhood of the other. 
+# min_samples: The number of samples (or total weight) in a neighborhood for a point to be considered as a core point.
+# leaf_size: Leaf size passed to BallTree or cKDTree. This can affect the speed of the construction and query, as well as the memory required to store the tree. 
+# 			 The optimal value depends on the nature of the problem.
+# n_jobs: The number of parallel jobs to run. None means 1 unless in a joblib.parallel_backend context. -1 means using all processors.
 dbscan = DBSCAN(eps=25, min_samples=5, leaf_size=30, n_jobs=4)
 
 y_pred = dbscan.fit_predict(pca_x_train)
@@ -28,7 +33,9 @@ y_pred = dbscan.fit_predict(pca_x_train)
 
 fig = plt.figure()
 sns.color_palette()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs=boston_dataframe[0], ys=boston_dataframe[1], zs=boston_dataframe[2], c=y_pred)
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(
+    xs=boston_dataframe[0], ys=boston_dataframe[1], zs=boston_dataframe[2], c=y_pred
+)
 plt.title("DBSCAN")
 plt.show()
